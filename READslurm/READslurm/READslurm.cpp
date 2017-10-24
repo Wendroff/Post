93f59fd93152817c8d9806f662b26bf42be77ecc
Wendroff <624101034@qq.com>
2017-10-24 10:27:23 +0800

    Improve the time computation method in READslum
    In the old version, when t was much bigger than dt, many time steps would have same t. Now the variable 'time' has added which is computed from iter and dt

--- READslurm/READslurm/READslurm.cpp
+++ READslurm/READslurm/READslurm.cpp
@@ -12,12 +12,14 @@ int main(int argc,char *argv[]){
 	//istringstream time; //用于从字符串“t=****”读取数字
 	//int       i=0;
 	double    Force;
-	double    t;
+	double    t,T;
+	int       iter;
+	double    dt;
 
 	//cout.precision(12);
 	output.precision(12);
 	//time.precision(12);
-	output<<"Variables = t ForceX"<<endl;
+	output<<"Variables = t iter dt Time ForceX"<<endl;
 	for (int i=1; i<argc; i++){
 		input.open(argv[i]);
 		while(input>>a){
@@ -28,13 +30,32 @@ int main(int argc,char *argv[]){
 				input>>a;
 				input>>Force;
 				while(input>>a){
-					if(a.compare(0,2,"t=") == 0){
+					if(a.compare(0,5,"iter=") == 0){
+						a.erase(0,5);
+						iter=stoi(a);
+						
+						input>>a;
+						if (a.compare(0,2,"t=") != 0){
+							throw runtime_error("iter= is not followed by a t= ");
+						}
 						a.erase(0,2);
 						t=stof(a);
+
+						input>>a;
+						if (a.compare(0,3,"dt=") != 0){
+							throw runtime_error("t= is not followed by a dt= ");
+						}
+						a.erase(0,3);
+						dt= stof(a);
+						T = dt * double(iter);
 						break;
 					}
 				}
-				output<<t<<"  "<<Force<<endl;
+				output<<t    <<"  "
+					  <<iter <<" "
+					  <<dt   <<" "
+					  <<T    <<" "
+					  <<Force<<endl;
 				//cout  <<t<<"  "<<Force<<endl;
 				//i++=;
 	//			cout<<(++i)<<endl;
